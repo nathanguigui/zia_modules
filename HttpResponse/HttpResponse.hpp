e2aa82e46b07f16f630a4b38f19aea95651ab432
Nathan GUIGUI <nathan.guigui@epitech.eu>
2020-01-29 16:09:47 +0100

    :sparkles: add base interface
    add base interface

---
+++ HttpResponse/HttpResponse.hpp
@@ -0,0 +1,19 @@
+//
+// Created by guigui on 1/29/20.
+//
+
+#ifndef CPP_RTYPE_2019_HTTPRESPONSE_HPP
+#define CPP_RTYPE_2019_HTTPRESPONSE_HPP
+
+#include "../HttpHeader/HttpHeader.hpp"
+#include <string>
+#include <vector>
+
+struct HttpResponse {
+    int httpMajorVersion;
+    int httpMinorVersion;
+    int responseCode;
+    std::string responseMessage;
+    std::vector<HttpHeader> headers;
+};
+#endif //CPP_RTYPE_2019_HTTPRESPONSE_HPP
---
+++ IModule/IModule.hpp
@@ -0,0 +1,31 @@
+//
+// Created by guigui on 1/29/20.
+//
+
+#ifndef CPP_RTYPE_2019_IMODULE_HPP
+#define CPP_RTYPE_2019_IMODULE_HPP
+
+#include "../HttpRequest/HttpRequest.hpp"
+#include "../HttpResponse/HttpResponse.hpp"
+
+class IModule {
+public:
+
+    /// Hook called when request came on server
+    virtual void handleRequest(HttpRequest &request) = 0;
+
+    /// Hook called before response
+    virtual void handlePreResponse(HttpRequest &request, HttpResponse &response) = 0;
+
+    /// Hook called after response
+    virtual void handlePostResponse(HttpRequest &request, HttpResponse &response) = 0;
+
+protected:
+};
+
+extern "C" IModule *create_object();
+
+extern "C" void destroy_object( IModule *object );
+
+
+#endif //CPP_RTYPE_2019_IMODULE_HPP
