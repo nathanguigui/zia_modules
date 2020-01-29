d93d22cbcb9cd50b19b6f95e6c8dc8b6da42c0d0
Nathan GUIGUI <nathan.guigui@epitech.eu>
2020-01-29 18:08:32 +0100

    :sparkles: add base exmaple + refactor
    add base exmaple + refactor

---
+++ example/LogModule/LogModule.hpp
@@ -0,0 +1,31 @@
+//
+// Created by guigui on 1/29/20.
+//
+
+#ifndef CPP_ZIA_2019_LOGMODULE_HPP
+#define CPP_ZIA_2019_LOGMODULE_HPP
+
+#include "zia_modules/includes/IModule/IModule.hpp"
+#include <iostream>
+
+class LogModule : public IModule {
+public:
+
+    LogModule();
+
+    virtual ~LogModule();
+
+    /// Hook called when request came on server
+    void handleRequest(HttpRequest &request) override;
+
+    /// Hook called before response
+    void handlePreResponse(HttpRequest &request, HttpResponse &response) override;
+
+    /// Hook called after response
+    void handlePostResponse(HttpRequest &request, HttpResponse &response) override;
+
+private:
+};
+
+
+#endif //CPP_ZIA_2019_LOGMODULE_HPP
